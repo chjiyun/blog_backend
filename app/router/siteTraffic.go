15f9efb0be54c0811369e1e955b24dd309afef15
chjiyun <lijiyun615@163.com>
2023-04-21 10:18:48 +0800

    fix 路由变动

--- app/router/siteTraffic.go
+++ app/router/siteTraffic.go
@@ -2,13 +2,14 @@ package router
 
 import (
 	"blog_backend/app/controller/siteTrafficController"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (r Router) SiteTraffic(g *gin.RouterGroup) {
 	rg := g.Group("/site-traffic")
 	{
-		rg.POST("/", siteTrafficController.AddSiteTraffic)
+		rg.POST("", siteTrafficController.AddSiteTraffic)
 		rg.GET("/pvs", siteTrafficController.GetManySiteTrafficPv)
 	}
 }
